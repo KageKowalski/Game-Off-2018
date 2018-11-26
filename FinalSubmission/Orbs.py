fc7a204148c89918ecbaa0942e997c2809b6c355
KageKowalski <kkowalski@mail.bradley.edu>
2018-11-26 15:12:13 -0600

    Failing miserably to squash the paddle-ball collision bug in orbs

--- FinalSubmission/Orbs.py
+++ FinalSubmission/Orbs.py
@@ -11,7 +11,7 @@ import math
 class Orbs:
     #Ball entity
     class Ball:
-        def __init__(self,_v=(random.random(),random.random()),_x=float(10),_y=float(10),_r=4,_c=random.randint(1,15)):
+        def __init__(self,_v,_x=float(192/2),_y=float(128/2),_r=4,_c=random.randint(1,15)):
             self.x = _x
             self.y = _y
             self.r = _r
@@ -34,7 +34,7 @@ class Orbs:
     
     #Initialize data
     def __init__(self):
-        self.balls = [self.Ball((random.random()+0.5,random.random()+0.5))]
+        self.balls = [self.Ball(((random.random()+0.5)/2,(random.random()+0.5)/2))]
         self.score = 0
         self.paddle = self.Paddle(0,0)
         self.game_over = False
@@ -42,41 +42,46 @@ class Orbs:
     
     #Update Orbs
     def update(self):
-        
         #Update game over menu
         if self.game_over:
-            
             #Return to hub if player presses 'B' key
             if pyxel.btnp(pyxel.KEY_B):
                 return True
         
         #Update game
         else:
-            
             #End game if player presses 'B' key
             if pyxel.btnp(pyxel.KEY_B):
                 self.game_over=True
             
+            #Move paddle
+            self.paddle = self.Paddle(pyxel.mouse_x,pyxel.mouse_y)
+            
             #Handle collisions
             for ball in self.balls:
-                
-                #Handle ball-window collisions
-                if ball.x + ball.r >= 160 or ball.x - ball.r <= 0:
-                    self.game_over = True
-                elif ball.y + ball.r >= 120 or ball.y - ball.r <= 0:
-                    self.game_over = True
-                    
-                #Handle ball-paddle collisions
-                elif math.sqrt( ((ball.x-self.paddle.ulc[0])**2)+((ball.y-self.paddle.ulc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.urc[0])**2)+((ball.y-self.paddle.urc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.llc[0])**2)+((ball.y-self.paddle.llc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.lrc[0])**2)+((ball.y-self.paddle.lrc[1])**2) ) <= ball.r:
-                    ball.v = (((random.random()-0.5)/3)-ball.v[0],((random.random() - 0.5)/3)-ball.v[1])
-                    self.score = self.score + 1
-                
                 #Move balls
                 ball.x = ball.x + ball.v[0]
                 ball.y = ball.y + ball.v[1]
                 
-            #Move paddle
-            self.paddle = self.Paddle(pyxel.mouse_x,pyxel.mouse_y)
+                #Handle ball-window collisions
+                if ball.x + ball.r >= pyxel.width or ball.x - ball.r <= 0:
+                    self.game_over = True
+                elif ball.y + ball.r >= pyxel.height or ball.y - ball.r <= 0:
+                    self.game_over = True
+                    
+                #Handle ball-paddle collisions
+                elif math.sqrt( ((ball.x-self.paddle.ulc[0])**2)+((ball.y-self.paddle.ulc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.urc[0])**2)+((ball.y-self.paddle.urc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.llc[0])**2)+((ball.y-self.paddle.llc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.lrc[0])**2)+((ball.y-self.paddle.lrc[1])**2) ) <= ball.r:
+                    ball.v = (((random.random()-0.5)/-ball.v[0],((random.random() - 0.5)/3)-ball.v[1]))
+                    self.score = self.score + 1
+                    
+                    #Move ball out of paddle
+                    while math.sqrt( ((ball.x-self.paddle.ulc[0])**2)+((ball.y-self.paddle.ulc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.urc[0])**2)+((ball.y-self.paddle.urc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.llc[0])**2)+((ball.y-self.paddle.llc[1])**2) ) <= ball.r or math.sqrt( ((ball.x-self.paddle.lrc[0])**2)+((ball.y-self.paddle.lrc[1])**2) ) <= ball.r:
+                        ball.x = ball.x + ball.v[0]
+                        ball.y = ball.y + ball.v[1]
+                    
+                    #Add new ball to game
+                    if self.score%25==0:
+                        self.balls.append(self.Ball(((random.random()+0.5)/2,(random.random()+0.5)/2)))
         
         return False
     
@@ -88,14 +93,12 @@ class Orbs:
         
         #Draw game over graphics
         if self.game_over:
-            
             #Draw game over and score text
             pyxel.text(8,8,"GAME OVER",2)
             pyxel.text(8,16,"Score: " + str(self.score),2)
         
         #Draw game graphics
         else:
-            
             #Draw paddle
             pyxel.rect(self.paddle.x-self.paddle.hl,self.paddle.y-self.paddle.hl,self.paddle.x+self.paddle.hl,self.paddle.y+self.paddle.hl,self.paddle.c)
             
