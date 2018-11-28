e2e783f31c5bb34cdf5349395dda80438c26f064
KageKowalski <kagekowalski@gmail.com>
2018-11-27 21:12:54 -0600

    Created PyxTrip grid

--- FinalSubmission/PyxTrip.py
+++ FinalSubmission/PyxTrip.py
@@ -3,13 +3,31 @@
 
 #imports
 import pyxel
+import enum
 
 
 #TEMPLATE game
 class PyxTrip:
+    class Entity(enum.Enum):
+        BLANK=0
+        PLAYER=-1
+        BLOCK=1
+    
+    
     #Initialize the game
     def __init__(self):
-        None #CODE TO INITILIZE GAME GOES HERE (DELETE NONE)
+        #Initialize variables
+        self.GRID_WIDTH=int(pyxel.width/8)
+        self.GRID_HEIGHT=3
+        self.grid=[]
+        
+        #Fill grid
+        for i in range(self.GRID_WIDTH):
+            self.grid.append([])
+            for j in range(self.GRID_HEIGHT):
+                self.grid[i].append(self.Entity.BLANK)
+        self.grid[int(self.GRID_WIDTH/3)][0]=self.Entity.PLAYER
+        self.grid[int(self.GRID_WIDTH/3)][1]=self.Entity.PLAYER
     
     
     #Update the game
@@ -18,7 +36,7 @@ class PyxTrip:
         if pyxel.btnp(pyxel.KEY_B):
             return True
         
-        #CODE TO UPDATE GAME GOES HERE
+        
         
         return False
     
